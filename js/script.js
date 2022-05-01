2facf7605bc108ffe07503e56eae53d649f5d380
Chinnathambi Annamalai <a.chinnathambi10@gmail.com>
2022-05-01 16:46:54 +0300

    Push project to remote

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function sendMessage(){
+    alert('Message sent successfully ');
+}
+function sayThankYou(){
+    alert('Thank you visiting!!!')
+}
+function changeLinkText(){
+    document.getElementById('link1').innerHTML='MY YOUTUBE CHANNEL LINK';
+}
